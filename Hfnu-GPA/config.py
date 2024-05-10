8477367948d0c73fcde6ef233f4570ea2a417bd7
夜雨 <2696227404@qq.com>
2024-05-10 10:20:30 +0800

    初始化配置参数 （可用发行版本）

--- Hfnu-GPA/config.py
+++ Hfnu-GPA/config.py
@@ -1,4 +1,4 @@
-cookie = "semester.id=145; JSESSIONID=E615F080C4865F43341F4A3AC08364E1; SERVERNAME=c4; GSESSIONID=E615F080C4865F43341F4A3AC08364E1"
+cookie = ""
 
-date_year = "2023-2024"
-date_semester = "1"
+date_year = ""
+date_semester = ""
